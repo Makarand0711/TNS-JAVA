dfe567507d2b695c02d335f62136542965382fd9
Hi <Hi@DESKTOP-3C7E7RG>
2025-07-02 13:39:47 +0530

    day 2

---
+++ Dhanu/src/com/tnsif/daytwo/jumpDemo/breack_demo.java
@@ -0,0 +1,18 @@
+package com.tnsif.daytwo.jumpDemo;
+
+public class breack_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5; i<10; i++)
+		{
+			if(i==5)
+				//break;
+				System.out.println(i);
+			System.out.println("HELLO FRINDES");
+			  //break;	
+		}
+
+	}
+
+}
---
+++ Dhanu/src/com/tnsif/daytwo/jumpDemo/scanner_demo.java
@@ -0,0 +1,11 @@
+
+package com.tnsif.daytwo.jumpDemo;
+
+public class scanner_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
