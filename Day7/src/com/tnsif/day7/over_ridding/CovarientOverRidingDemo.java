91178085f168cf02c0352cf7088e94c78160dde6
Hi <Hi@DESKTOP-3C7E7RG.bwrouter>
2025-07-09 23:05:43 +0530

    day 7

--- Day7/src/com/tnsif/day7/over_ridding/CovarientOverRidingDemo.java
+++ Day7/src/com/tnsif/day7/over_ridding/CovarientOverRidingDemo.java
@@ -36,7 +36,7 @@ class Blue extends Colour{
 
 
 
-public class OverRidingDemo {
+public class CovarientOverRidingDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
